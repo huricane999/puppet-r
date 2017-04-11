4f7a3f404cecb55807aa7a37d4e8067dbcf6b29f
Chris Smith <casmit14@ncsu.edu>
2017-04-11 11:54:47 -0400

    add install_github support for RedHat/Debian

---
+++ manifests/github_package.pp
@@ -0,0 +1,57 @@
+# github_package.pp
+
+define r::github_package (
+  $r_path       = '',
+  $dependencies = false,
+  $environment  = undef,
+  $timeout      = 300,
+  $organization = '',
+) {
+  if $name =~ /.*\/.*/ {
+    $name_parts = split($name, '/')
+  } else {
+    $name_parts = nil
+  }
+
+  if $organization != '' {
+    $org = $organization
+  } elsif $name_parts != nil {
+    $org = $name_parts[0]
+  }
+
+  if $name_parts == nil  {
+    $package_name = $name
+  } else {
+    $package_name = $name_parts[1]
+  }
+
+  $deps = $dependencies ? {
+    true    => 'TRUE',
+    default => 'FALSE'
+  }
+
+  case $::osfamily {
+    'Debian', 'RedHat': {
+
+      if $r_path == '' {
+        $binary = '/usr/bin/R'
+      }
+      else
+      {
+        $binary = $r_path
+      }
+
+      $command = "${binary} -e \"library(devtools); install_github('${package_name}', username = '${org}', dependencies = ${deps})\""
+
+        exec { "install_r_package_${name}":
+          command     => $command,
+          environment => $environment,
+          timeout     => $timeout,
+          unless      => "${binary} -q -e \"'${package_name}' %in% installed.packages()\" | grep 'TRUE'",
+          require     => Class['r']
+        }
+
+    }
+    default: { fail("Not supported on osfamily ${::osfamily}") }
+  }
+}
